d75e0d5f4559f85b6463f8227170ed8be8314cf1
unknown <nitishrawat009@gmail.com>
2024-04-17 23:53:54 +0530

    added an AC soln

--- strings/check-if-a-string-can-break-another-string.cpp
+++ strings/check-if-a-string-can-break-another-string.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
+    bool cmp(string s1, string s2){
+        int n = s1.size();
+        for(int i = 0; i < n; i++){
+            if(s1[i] < s2[i]){ return false; }
+        }
+        return true;
+    }
     bool checkIfCanBreak(string s1, string s2) {
-        
+        sort(s1.begin(), s1.end());
+        sort(s2.begin(), s2.end());
+
+        return (cmp(s1,s2) || cmp(s2,s1));
     }
 };
