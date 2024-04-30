9c120dbdcd7813d96cf8368c5d92eac54d57d1ba
unknown <nitishrawat009@gmail.com>
2024-04-30 23:46:11 +0530

    problem 806 AC

--- strings/number-of-lines-to-write-string.cpp
+++ strings/number-of-lines-to-write-string.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     vector<int> numberOfLines(vector<int>& widths, string s) {
-        vector<int> lines = {0, 0};
-
+        vector<int> lines{0, 0};
+        
         int curr = 0;
         for(int i = 0; i < s.size(); i++){
-            int temp = curr + widths[s[i]-65];
-            if(curr > 100){
-                i--;
+            
+            int temp = widths[int(s[i])-int('a')];
+            if((curr+temp) > 100){
                 lines[0]++;
                 lines[1] = curr;
                 curr = 0;
-                continue;
             } 
             curr += temp;
         }
-
+       
+        lines[0]++;
+        lines[1] = curr;
+        
         return lines;
     }
 };
