057ce0681b5a7e0af3b5cd1c3f7fc7968b1a16d8
unknown <nitishrawat009@gmail.com>
2024-05-29 22:23:37 +0530

    Leetcode 2824

---
+++ arrays/count-pairs-whose-sum-is-less-than-target.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        
+        sort(nums.begin(), nums.end());
+        int counter = 0;
+        for(int i = 0; i <nums.size(); i++){
+            for(int j = i+1; j < nums.size(); j++){
+                if( (nums[i]+nums[j]) >= target){
+                    break;
+                }
+
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+};
